7cfee9139b52c34466fef3852361639f2103b4b6
Manoj Patil <manoj.patil04@gmail.com>
2021-09-23 16:14:57 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,48 +1,85 @@
 import json
 import boto3
+import pymysql
+import logging
 import csv
-import mysql.connector
-import os
 
-s3 = boto3.client('s3')
-rds_endpoint  = 'database-1.csc5q5bwthqk.ap-south-1.rds.amazonaws.com'
-username = 'admin'  #username for RDS Mysql
-password = 'password'  # RDS Mysql password
-db_name = 'demodatabase'  # RDS MySQL DB name
-conn = None
+s3=boto3.client('s3')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-bucket=''
-key=''
-def lambda_handler(event,context):
-        for record in event['Records']:
-                bucket = record['s3']['bucket']['name']
-                key = record['s3']['object']['key'] 
-        
-        db = mysql.connector.connect(   host = rds_endpoint,
-                        user = username,
-                        passwd = password,
-                        db = db_name)
-        cursor = db.cursor()
+db_endpoint = 'DB_END_POINT'
+db_user = 'db_User'
+db_password = 'db_Password'
+db_name = 'db_database'
+port=3306
 
-        query = 'INSERT INTO test(name,email,department) VALUES(%s, %s, %s)'                                                         
+def check_file_exists(uploaded_filename_csv):
+    try:
+        uploaded_filename_csv=uploaded_filename_csv.replace(".csv","")
+        connection=pymysql.connect(host=db_endpoint, user=db_user, password=db_password,database=db_name,port=port)    
+        cursor = connection.cursor()
+        #query="SELECT count(*) FROM information_schema.TABLES WHERE (TABLE_SCHEMA = 'demodatabase') AND (TABLE_NAME = '"+filename+"')"
+        count_table = cursor.execute( "SHOW TABLES LIKE '%"+uploaded_filename_csv+"%'")
+        return count_table
+    except Exception as e:
+        print('error')
+
+def create_new_table(bucket_name,uploaded_filename_csv,table_schema):
+    try:
+        connection=pymysql.connect(host=db_endpoint, user=db_user, password=db_password,database=db_name,port=port)    
+        cursor = connection.cursor()
+        with open('/tmp/'+table_schema) as file:
+            lines = file.readlines()
+            lines=''.join(map(str, lines))
+            lines=lines.replace("[","")
+            lines=lines.replace("]","")
+            lines=lines.replace("%20"," ")
+            create_table_query = lines
+            cursor.execute(create_table_query)
+            connection.commit()
+            logger.info('Table Created in RDS')
+            insert_into_RDS(bucket_name,uploaded_filename_csv,table_schema)
+    except Exception as e:
+        logger.info('Table Creation Failed !!')
+        logger.info(e) 
+
+def insert_into_RDS(bucket_name,uploaded_filename_csv,table_schema):
+        connection = pymysql.connect(host=db_endpoint,user=db_user,password=db_password,db=db_name,port=port)
+        cursor = connection.cursor()
+        uploaded_filename_csv=uploaded_filename_csv.replace(".csv","")
+        cursor.execute("select column_name from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='"+uploaded_filename_csv+"'")
+        column_names = cursor.fetchall()
+        column_names = str(column_names)
+        column_names=column_names.replace("(''","")
+        column_names=column_names.replace("',)","")
+        column_names=column_names.replace("('","")
+        column_names=column_names.replace("',","")
+        query = 'INSERT INTO '+uploaded_filename_csv+column_names+' VALUES(%s, %s, %s)'                                                         
         my_data = []
-        with open(key) as csv_file:
+        with open('/tmp/'+uploaded_filename_csv+'.csv') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
                 for row in csv_reader:
                         my_data.append(tuple(row))
         try:
-                # Executing the SQL command
+                my_data.pop(0)
                 cursor.executemany(query,my_data)
-                # Commit your changes in the database
-                db.commit()
+                connection.commit()
                 print('commit')
-
         except:
-                # Rolling back in case of error
-                db.rollback()
-                print('rollback')
-
-        print("Data inserted")
-
-        # Closing the connection
-        db.close()
+                connection.rollback()
+                print('rollback done')
+        connection.close()
+        
+def lambda_handler(event,context):
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    uploaded_filename_csv = event['Records'][0]['s3']['object']['key']
+    table_schema = uploaded_filename_csv
+    table_schema=table_schema.replace(".csv",".json")
+    s3.download_file(bucket_name,uploaded_filename_csv,"/tmp/"+uploaded_filename_csv)
+    s3.download_file(bucket_name,table_schema,"/tmp/"+table_schema)
+    count_table=check_file_exists(uploaded_filename_csv)
+    if count_table == 1:
+        insert_into_RDS(bucket_name,uploaded_filename_csv,table_schema)
+    else:
+        create_new_table(bucket_name,uploaded_filename_csv,table_schema)
